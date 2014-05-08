eae231837542b85562f013accb54f70fa2884cc0
George Deglin <george@deglin.com>
2014-05-07 22:18:08 -0700

    Initial Plugin Commit

---
+++ Example/push-notifications/AppDelegate.m
@@ -0,0 +1,86 @@
+/**
+ * Copyright 2014 GameThrive
+ * Portions Copyright 2014 StackMob
+ *
+ * This file includes portions from the StackMob iOS SDK and distributed under an Apache 2.0 license.
+ * StackMob was acquired by PayPal and ceased operation on May 22, 2014.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+@synthesize gameThrive = _gameThrive;
+
+- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.gameThrive = [[GameThrive alloc] init];
+    
+    return YES;
+}
+
+- (void)application:(UIApplication*)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken {
+    [self.gameThrive registerDeviceToken:deviceToken onSuccess:^(NSDictionary* results) {
+        NSLog(@"Device Registered with GameThrive.");
+    } onFailure:^(NSError* error) {
+        NSLog(@"Device Registion Error with GameThrive: %@", error);
+    }];
+}
+
+- (void)application:(UIApplication*)app didFailToRegisterForRemoteNotificationsWithError:(NSError*)err {
+    NSLog(@"Error in registration. Error: %@", err);
+}
+
+- (void)application:(UIApplication*)application didReceiveRemoteNotification:(NSDictionary*)userInfo {
+    // notificationOpened must be called here. Everything else below is optional in your game.
+    [self.gameThrive notificationOpened:userInfo];
+    
+    NSString* message = [self.gameThrive getMessageString];
+    NSDictionary* additionalData = [self.gameThrive getAdditionalData];
+    
+    UIAlertView* alertView;
+    
+    if (additionalData != nil) {
+        // Append AdditionalData at the end of the message
+        message = [message stringByAppendingString:[NSString stringWithFormat:@":data:%@", additionalData]];
+        
+        NSString* messageTitle;
+        if ([additionalData objectForKey:@"discount"] != nil)
+            messageTitle = @"Discount!";
+        else if ([additionalData objectForKey:@"bonusCredits"] != nil)
+            messageTitle = @"Bonus Credits!";
+        else
+            messageTitle = @"Other Extra Data";
+        
+        alertView = [[UIAlertView alloc] initWithTitle:messageTitle
+                                            message:message
+                                            delegate:self
+                                            cancelButtonTitle:@"Close"
+                                            otherButtonTitles:@"Show", nil];
+    }
+    
+    // If a push notification is received when the app is being used it does not go to the notifiction center so display in your app.
+    if (alertView == nil && [application applicationState] == UIApplicationStateActive) {
+        alertView = [[UIAlertView alloc] initWithTitle:@"GameThrive Message"
+                                        message:message
+                                        delegate:self
+                                        cancelButtonTitle:@"Close"
+                                        otherButtonTitles:@"Show", nil];
+    }
+    
+    // Add your game logic around this so the user is not interrupted during gameplay.
+    if (alertView != nil)
+        [alertView show];
+}
+
+@end
---
+++ iOS_SDK/GameThrive/GameThrive.m
@@ -0,0 +1,276 @@
+/**
+ * Copyright 2014 GameThrive
+ * Portions Copyright 2014 StackMob
+ *
+ * This file includes portions from the StackMob iOS SDK and distributed under an Apache 2.0 license.
+ * StackMob was acquired by PayPal and ceased operation on May 22, 2014.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#if TARGET_OS_IPHONE
+#import <UIKit/UIKit.h>
+#endif
+
+#import "GameThrive.h"
+#import "GTHTTPClient.h"
+#import "AFJSONRequestOperation.h"
+#import <stdlib.h>
+#import <stdio.h>
+#import <sys/types.h>
+#import <sys/sysctl.h>
+#import <sys/utsname.h>
+#import <UIKit/UIKit.h>
+
+#define DEFAULT_PUSH_HOST @"https://gamethrive.com/api/v1/"
+
+static GameThrive *defaultClient = nil;
+
+@interface GameThrive ()
+
+@property(nonatomic, readwrite, copy) NSString *app_id;
+@property(nonatomic, readwrite, copy) NSDictionary *lastMessageReceived;
+@property(nonatomic, readwrite, copy) NSString *deviceModel;
+@property(nonatomic, readwrite, copy) NSString *systemVersion;
+@property(nonatomic, retain) GTHTTPClient *httpClient;
+
+@end
+
+@implementation GameThrive
+
+@synthesize app_id = _GT_publicKey;
+@synthesize httpClient = _GT_httpRequest;
+@synthesize lastMessageReceived;
+
+NSMutableDictionary* tagsToSend;
+NSString* deviceToken;
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        self.app_id = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"GameThrive_APPID"];
+        NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@", DEFAULT_PUSH_HOST]];
+        self.httpClient = [[GTHTTPClient alloc] initWithBaseURL:url];
+//#if TARGET_OS_MAC
+//        /* Get this for mac */
+//        self.systemVersion = [[NSProcessInfo processInfo] operatingSystemVersionString];
+//        
+//        size_t len = 0;
+//        sysctlbyname("hw.model", NULL, &len, NULL, 0);
+//        if(len)
+//        {
+//            char *model = malloc(len*sizeof(char));
+//            sysctlbyname("hw.model", model, &len, NULL, 0);
+//            
+//            self.deviceModel = [[NSString alloc] initWithCString:model encoding:NSUTF8StringEncoding];
+//            
+//            free(model);
+//        }
+//#else
+        struct utsname systemInfo;
+        uname(&systemInfo);
+        self.deviceModel   = [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
+        self.systemVersion = [[UIDevice currentDevice] systemVersion];
+//#endif
+        
+        if ([GameThrive defaultClient] == nil)
+            [GameThrive setDefaultClient:self];
+        
+        // Handle changes to the app id. This might happen on a developer's device when testing.
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        if (![self.app_id isEqualToString:[defaults stringForKey:@"GT_APP_ID"]]) {
+            [defaults setObject:self.app_id forKey:@"GT_APP_ID"];
+            [defaults setObject:nil forKey:@"GT_PLAYER_ID"];
+            [defaults synchronize];
+        }
+        
+        // Register this device with Apple's APNS server.
+        // Calls didRegisterForRemoteNotificationsWithDeviceToken in AppDelegate.m in their app,
+        // which in turns calls back into our SDK via the registerDeviceToken method.
+        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:UIRemoteNotificationTypeAlert];
+    }
+    
+    [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
+    
+    return self;
+}
+
+- (void)registerDeviceToken:(id)inDeviceToken onSuccess:(GTResultSuccessBlock)successBlock onFailure:(GTFailureBlock)failureBlock {
+    NSString* deviceToken = [[inDeviceToken description] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]];
+    deviceToken = [[deviceToken componentsSeparatedByString:@" "] componentsJoinedByString:@""];
+    
+    NSString* playerId = [[NSUserDefaults standardUserDefaults] stringForKey:@"GT_PLAYER_ID"];
+    
+    NSMutableURLRequest *request;
+    if (playerId == nil)
+        request = [self.httpClient requestWithMethod:@"POST" path:@"players" parameters:nil];
+    else
+        request = [self.httpClient requestWithMethod:@"POST" path:[NSString stringWithFormat:@"players/%@/on_session", playerId]  parameters:nil];
+    
+    NSDictionary *infoDictionary = [[NSBundle mainBundle]infoDictionary];
+    NSString *build = infoDictionary[(NSString*)kCFBundleVersionKey];
+    
+    NSDictionary *dataDic = [NSDictionary dictionaryWithObjectsAndKeys:
+                             deviceToken, @"identifier",
+                             self.app_id, @"app_id",
+                             self.deviceModel, @"device_model",
+                             self.systemVersion, @"device_os",
+                             [[NSLocale preferredLanguages] objectAtIndex:0], @"language",
+                             [NSNumber numberWithInt:(int)[[NSTimeZone localTimeZone] secondsFromGMT]], @"timezone",
+                             build, @"game_version",
+                             [NSNumber numberWithInt:0], @"device_type",
+                             nil];
+    
+    NSData *postData = [NSJSONSerialization dataWithJSONObject:dataDic options:0 error:nil];
+    [request setHTTPBody:postData];
+    
+    [self enqueueRequest:request onSuccess:^(NSDictionary *results) {
+        if ([results objectForKey:@"id"] != nil) {
+            NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+            [defaults setObject:[results objectForKey:@"id"] forKey:@"GT_PLAYER_ID"];
+            [defaults synchronize];
+            
+            if (tagsToSend != nil) {
+                [self sendTags:tagsToSend];
+                tagsToSend = nil;
+            }
+        }
+        successBlock(results);
+    } onFailure:failureBlock];
+}
+
+- (NSString*)getPlayerId {
+    return [[NSUserDefaults standardUserDefaults] stringForKey:@"GT_PLAYER_ID"];
+}
+
+- (NSString*)getDeviceToken {
+    return deviceToken;
+}
+
+- (void)sendTags:(NSDictionary*)keyValuePair {
+    [self sendTags:keyValuePair onSuccess:nil onFailure:nil];
+}
+
+- (void)sendTags:(NSDictionary*)keyValuePair onSuccess:(GTResultSuccessBlock)successBlock onFailure:(GTFailureBlock)failureBlock {
+    NSString* playerId = [[NSUserDefaults standardUserDefaults] stringForKey:@"GT_PLAYER_ID"];
+    
+    if (playerId == nil) {
+        if (tagsToSend == nil)
+            tagsToSend = [keyValuePair mutableCopy];
+        else
+            [tagsToSend addEntriesFromDictionary:keyValuePair];
+        return;
+    }
+    
+    NSMutableURLRequest* request = [self.httpClient requestWithMethod:@"PUT" path:[NSString stringWithFormat:@"players/%@", playerId]  parameters:nil];
+    
+    NSDictionary *dataDic = [NSDictionary dictionaryWithObjectsAndKeys:
+                             self.app_id, @"app_id",
+                             keyValuePair, @"tags",
+                             nil];
+    
+    NSData *postData = [NSJSONSerialization dataWithJSONObject:dataDic options:0 error:nil];
+    [request setHTTPBody:postData];
+    
+    [self enqueueRequest:request
+               onSuccess:successBlock
+               onFailure:failureBlock];
+}
+
+- (void)sendTag:(NSString*)key value:(NSString*)value {
+    [self sendTag:key value:value onSuccess:nil onFailure:nil];
+}
+
+- (void)sendTag:(NSString*)key value:(NSString*)value onSuccess:(GTResultSuccessBlock)successBlock onFailure:(GTFailureBlock)failureBlock {
+    [self sendTags:[NSDictionary dictionaryWithObjectsAndKeys: value, key, nil] onSuccess:successBlock onFailure:failureBlock];
+}
+
+- (void)sendPurchase:(NSNumber*)amount onSuccess:(GTResultSuccessBlock)successBlock onFailure:(GTFailureBlock)failureBlock {
+    NSString* playerId = [[NSUserDefaults standardUserDefaults] stringForKey:@"GT_PLAYER_ID"];
+    
+    NSMutableURLRequest* request = [self.httpClient requestWithMethod:@"POST" path:[NSString stringWithFormat:@"players/%@/on_purchase", playerId]  parameters:nil];
+    
+    NSDictionary *dataDic = [NSDictionary dictionaryWithObjectsAndKeys:
+                             self.app_id, @"app_id",
+                             amount, @"amount",
+                             nil];
+    
+    NSData *postData = [NSJSONSerialization dataWithJSONObject:dataDic options:0 error:nil];
+    [request setHTTPBody:postData];
+    
+    [self enqueueRequest:request
+               onSuccess:successBlock
+               onFailure:failureBlock];
+}
+- (void)sendPurchase:(NSNumber*)amount {
+    [self sendPurchase:amount onSuccess:nil onFailure:nil];
+}
+
+- (void)notificationOpened:(NSDictionary*)messageDict {
+    NSDictionary* customDict = [messageDict objectForKey:@"custom"];
+    NSString* messageId = [customDict objectForKey:@"i"];
+    
+    NSMutableURLRequest* request = [self.httpClient requestWithMethod:@"PUT" path:[NSString stringWithFormat:@"notifications/%@", messageId]  parameters:nil];
+    
+    NSDictionary *dataDic = [NSDictionary dictionaryWithObjectsAndKeys:
+                             self.app_id, @"app_id",
+                             @(YES), @"opened",
+                             nil];
+    
+    NSData *postData = [NSJSONSerialization dataWithJSONObject:dataDic options:0 error:nil];
+    [request setHTTPBody:postData];
+    
+    [self enqueueRequest:request onSuccess:nil onFailure:nil];
+    
+    if ([customDict objectForKey:@"u"] != nil) {
+        NSURL *url = [NSURL URLWithString:[customDict objectForKey:@"u"]];
+        [[UIApplication sharedApplication] openURL:url];
+    }
+    
+    self.lastMessageReceived = messageDict;
+    
+    // Clear bages and nofiications from this app. Setting to 1 then 0 was needed to clear the notifications.
+    [[UIApplication sharedApplication] setApplicationIconBadgeNumber:1];
+    [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
+    [[UIApplication sharedApplication] cancelAllLocalNotifications];
+}
+
+- (NSDictionary*)getAdditionalData {
+    return [[self.lastMessageReceived objectForKey:@"custom"] objectForKey:@"a"];
+}
+
+- (NSString*)getMessageString {
+    return [[self.lastMessageReceived objectForKey:@"aps"] objectForKey:@"alert"];
+}
+
+- (void)enqueueRequest:(NSURLRequest *)request onSuccess:(GTResultSuccessBlock)successBlock onFailure:(GTFailureBlock)failureBlock {
+    AFJSONRequestOperation *op = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *urlRequest, NSHTTPURLResponse *response, id JSON) {
+        if (successBlock != nil)
+            successBlock(JSON);
+    }failure:^(NSURLRequest *urlRequest, NSHTTPURLResponse *response, NSError *error, id JSON) {
+        if (failureBlock != nil)
+            failureBlock([NSError errorWithDomain:@"GTError" code:[response statusCode] userInfo:JSON]);
+    }];
+    [self.httpClient enqueueHTTPRequestOperation:op];  
+}
+
+
++ (void)setDefaultClient:(GameThrive *)client {
+    defaultClient = client;
+}
+
++ (GameThrive *)defaultClient {
+    return defaultClient;
+}
+
+@end
